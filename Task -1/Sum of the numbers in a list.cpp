49cf164c9a22958e006590f694cc9e3b24d1fd5d
Mahmoud Said <mahmoueddragon2004@gmail.com>
2022-11-11 23:05:28 +0200

    Add files via upload

---
+++ Task -1/Sum of the numbers in a list.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int Num, Sum = 0, i = 0;
+
+	cout << "Enter Number of digits: ";
+	cin >> Num;
+
+	int list[Num];
+	
+	//for loop
+
+	for (i; i < Num; i++) {
+		cin >> list[i];
+
+		Sum = Sum + list[i];
+	}
+	cout << "Sum using for loop = " << Sum << endl;
+
+	//------------------------
+
+	//while loop
+
+	while (i < Num) {
+
+		cin >> list[i];
+
+		Sum = Sum + list[i];
+		i++;
+	}
+	cout << "Sum using while loop = " << Sum << endl;
+}
+
